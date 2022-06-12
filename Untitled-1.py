509f77f7b3e2f2703faf06ee2967e33446076ae5
Alphablaze72 <73984425+Alphablaze72@users.noreply.github.com>
2022-06-12 23:03:01 +0530

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -10,31 +10,31 @@ def main(num):
             print('wrong input!!!')
         computer_move = random.choice(moves)
         winner(user_input, computer_move)
-        for player in score:
-            print(player+':'+score[player])
+    for player in score:
+        print(str(player)+':'+str(score[player]))
 def winner(user_input, computer_move):
     if user_input == computer_move:
         print("Both of the parties have chosen the same move:"+ user_input+ 'so it is a tie')
-    if user_input == 'p':
+    elif user_input == 'p':
         if computer_move == 's':
             print("sciccors cut paper:  Computer wins")
-            score['commputer'] +=1 
+            score['computer'] +=1
         else:
             print('Paper covers rock, User wins')
-            score['user'] +=1 
+
+            score['user'] +=1
     elif user_input == 'r':
         if computer_move == 'p':
             print("Paper covers rock Computer wins")
-            score['commputer'] +=1 
+            score['computer'] +=1
         else:
             print('Rock smasshes scissors, User wins')
-            score['user'] +=1 
+            score['user'] +=1
     else:
         if computer_move == 'r':
             print("Rock smasshes scissors:  Computer wins")
-            score['commputer'] +=1 
+            score['computer'] +=1
         else:
             print('scissors cut paper, User wins')
-            score['user'] +=1 
-
-       
+            score['user'] +=1
+main(num)
